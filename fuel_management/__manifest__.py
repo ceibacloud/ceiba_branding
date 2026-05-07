cfa69cce2bf60224e0199b4cd1274bff4bff2cef
BSH Admin <bshadmin@BSH-PP-LAB001-2.local>
2026-05-06 19:09:51 -0600

    Add fuel_management module with gas station centric login

---
+++ fuel_management/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Fuel Management Branding',
+    'version': '18.0.1.0.0',
+    'category': 'Hidden',
+    'summary': 'Gas station centric branding and UI for Ceiba Clouds',
+    'author': 'Ceiba Clouds',
+    'website': 'https://www.ceibaclouds.com',
+    'license': 'LGPL-3',
+    'depends': [
+        'web',
+        'ceiba_brand_core',
+    ],
+    'data': [
+        'views/login_templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
